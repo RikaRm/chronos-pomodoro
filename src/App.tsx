3cf6d7272eee3d2d982eded870ca24b151cde9fc
Ricardo Macedo <rikmac2002@gmail.com>
2025-08-24 23:04:36 -0300

    chore: padronizando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './styles/themes.css';
 import './styles/global.css';
 import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 function App() {
   return (
     <>
       <Container>
+        <Heading>Nego ney</Heading>
         <section>LOGO</section>
       </Container>
 
       <Container>
+        <Heading>Nego ney</Heading>
         <section>MENU</section>
-      </Container>
-
-      <Container>
-        <section>FORM</section>
-      </Container>
-
-      <Container>
-        <section>FOOTER</section>
       </Container>
     </>
   );
